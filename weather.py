308d6386c89d547bd99c424607d9250f97666275
sbikov <slava.bikov@intel.com>
2021-03-23 17:53:38 +0200

    next

--- weather.py
+++ weather.py
@@ -2,5 +2,8 @@ import pyowm
 
 APIKEY='7045f4addca69a18ddabc4c10d413d8a' 
 owm = pyowm.OWM(APIKEY) # TODO: Replace <api_key> with your API key
-la = owm.three_hours_forecast('Los Angeles, US')
-print(la.will_have_clouds())
+
+
+sf = owm.weather_at_place('San Francisco, US')
+weather = sf.get_weather()
+print(weather.get_temperature('fahrenheit')['temp'])
