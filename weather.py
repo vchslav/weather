4257c671aa00342f8dce1da11292c4ca133aa26f
sbikov <slava.bikov@intel.com>
2021-03-23 18:16:04 +0200

    next

--- weather.py
+++ weather.py
@@ -5,8 +5,10 @@ from pyowm.utils import timestamps
 owm = OWM('7045f4addca69a18ddabc4c10d413d8a')
 mgr = owm.weather_manager()
 
-observation = mgr.weather_at_place('London,GB')
+observation = mgr.weather_at_place('Tel-Aviv,IL')
 w = observation.weather
+
+for i in w:
 
 print(w.temperature('celsius'))
 
