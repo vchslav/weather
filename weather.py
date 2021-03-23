58da3b3bc222f8a35eac8ee5622b47a3bad06d54
sbikov <slava.bikov@intel.com>
2021-03-23 17:50:32 +0200

    next

--- weather.py
+++ weather.py
@@ -1,11 +1,4 @@
-import pyowm # import Python Open Weather Map to our project.
-
-APIKEY='7045f4addca69a18ddabc4c10d413d8a'                  #your API Key here as string
-OpenWMap=pyowm.OWM(APIKEY)                   # Use API key to get data
-mng = OpenWMap.weather_manager()    #NEW
-obs = mng.weather_at_place('Tel-Aviv')
-#Weather=OpenWMap.weather_at_place(Tel-Aviv)  # give where you need to see the weather
-#Data=Weather.get_weather()                 # get out data in the mentioned location
-NewData=obs.get_weather()  
-temp=NewData.get_temperature(unit='celsius') #NEW
-print ("Average Temp. Currently ", temp['temp']) # get avg. tmp
+import pyowm
+owm = pyowm.OWM('<api_key>') # TODO: Replace <api_key> with your API key
+la = owm.three_hours_forecast('Los Angeles, US')
+print(la.will_have_clouds())
