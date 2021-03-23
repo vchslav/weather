6ba054e70d62207ba30c02a680568910922796c9
sbikov <slava.bikov@intel.com>
2021-03-23 18:01:47 +0200

    next

--- weather.py
+++ weather.py
@@ -4,6 +4,5 @@ APIKEY='7045f4addca69a18ddabc4c10d413d8a'
 owm = OWM(APIKEY) # TODO: Replace <api_key> with your API key
 mgr = owm.weather_manager()
 observation = mgr.weather_at_place('San Francisco, US')
-weather = observation.get_weather()
-print(weather.get_temperature('fahrenheit')['temp'])
+print(observation)
 
