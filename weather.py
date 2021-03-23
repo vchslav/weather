2237ee01473ca5697ed1f215d13a6e76d15e4607
sbikov <slava.bikov@intel.com>
2021-03-23 17:20:48 +0200

    init

---
+++ weather.py
@@ -0,0 +1,9 @@
+import pyowm # import Python Open Weather Map to our project.
+
+APIKEY='7045f4addca69a18ddabc4c10d413d8a'                  #your API Key here as string
+OpenWMap=pyowm.OWM(APIKEY)                   # Use API key to get data
+mng = OpenWMap.weather_manager()    #NEW
+obs = mng.weather_at_place('Tel-Aviv')
+#Weather=OpenWMap.weather_at_place(Tel-Aviv)  # give where you need to see the weather
+#Data=Weather.get_weather()                 # get out data in the mentioned location
+print(obs)
