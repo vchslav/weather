27a526b002e7d547108a34908622d47998b87f6f
sbikov <slava.bikov@intel.com>
2021-03-23 21:15:43 +0200

    next

--- weather.py
+++ weather.py
@@ -14,8 +14,7 @@ def get_curr_temp():
     temp = w.temperature('celsius')
     weather_parameters = ("detailed_status", "wind()", "temperature('celsius')", "rain", "heat_index", "clouds")
     for x in weather_parameters:
-        z = "w."+str(variable)
-        print(z)
+        print(x)
     return temp
 
 
